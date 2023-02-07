18c7adae2f51cce174e6392c457cf2205f871de0
Mollaev Semur <mollaev.semur@gmail.com>
2023-02-07 16:35:36 +0300

    Done!

--- react-first/src/App.js
+++ react-first/src/App.js
@@ -1,9 +1,27 @@
 import React, { useState } from "react";
 
 function App() {
+  console.log('Hi')
+  const [count, setCount] = useState(0)
+
+  const increment = () => {
+    setCount(count + 1)
+  }
+
+  const decrement = () => {
+    if (count > 1) {
+      setCount(count - 1)
+    }
+
+  }
+
   return (
     <>
-      Hi
+      <h2> Hello world </h2>
+      <p> Respect to react: </p>
+      <div> {count} </div>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
     </>
   )
 }
